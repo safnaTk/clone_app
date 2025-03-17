91688e4d98186dbc784bff805859118e483b3679
safnaTk <safnatk697@gmail.com>
2025-03-18 00:34:30 +0530

    remove a coment

--- lib/utils/constants/image_constants.dart
+++ lib/utils/constants/image_constants.dart
@@ -1,6 +1,6 @@
 class ImageConstants {
   static const String splashimage =
-      "Assets/images/splashimge.jpeg"; //static-single memory allocation
+  "Assets/images/splashimge.jpeg"; //static-single memory allocation
   static const String logoimage = "Assets/images/logo.png";
-  // static const String partnersimage = "Assets/images/partners img.jpg";
+  
 }
